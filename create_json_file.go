c2cacf50e2b38b27cc711ea32d09c222b38b6795
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2019-09-11 23:53:29 +0900

    add json create program

---
+++ create_json_file.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Person struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"` // nameというキーで格納
+	Email   string `json:"-"`    // JSONに格納しない
+	Age     int    `json:"age"`
+	Address string `json:"address,omitempty"` // 値が空なら出力しない
+	memo    string
+}
+
+func main() {
+	person := &Person{
+		ID:      1,
+		Name:    "Gopher",
+		Email:   "gopher@example.org",
+		Age:     5,
+		Address: "",
+		memo:    "golang lover",
+	}
+
+	// ファイルを開く
+	file, err := os.Create("./create_by_go.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// エンコーダの取得
+	encoder := json.NewEncoder(file)
+
+	// JSONエンコードしたデータの書き込み
+	err = encoder.Encode(person)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
